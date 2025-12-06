1136c7650df50c150cc8874e12fcbf51ddc8fa97
AjayKumar882 <ajaykumartilkar59.com>
2025-12-06 14:18:20 +0530

    reverse_String.cpp

--- reverse_String.cpp
+++ reverse_String.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string 
+    string s="hello";
+    int i=0;
+    int j=s.size()-1;
+
+    while(i<j){
+        swap(s[i],s[j]);
+        i++;
+        j--;
+    }
+    cout<<s;
 }
