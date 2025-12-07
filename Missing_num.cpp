a205ae2c4448ebba25edc3b913a4945b7e0c65c9
AjayKumar882 <ajaykumartilkar59.com>
2025-12-07 14:51:01 +0530

    update

--- Missing_num.cpp
+++ Missing_num.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main(){
     vector<int>v={1,2,3,5,6,7};
-    int sum=0;
+    
     int n = v.size() + 1;
     int indexsum = n * (n + 1) / 2;
+    int sum=0;
 
-    for(int i=0; i<v.size(); i++){
+    for(int i=0; i<n-1; i++){
         sum+=v[i];
     }
     cout<< "missing element is : "<< indexsum-sum;
